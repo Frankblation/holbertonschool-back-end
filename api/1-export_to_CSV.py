cc37af81f1dc679d1e421b39a964ae45de18a737
Frankblation <124920027+Frankblation@users.noreply.github.com>
2023-11-21 21:52:35 -0600

    1

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -11,21 +11,36 @@ def export_to_csv(employee_id, tasks):
 
     user_url = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
     user_request = requests.get(user_url).json()
+
+    # Verify that the user ID in the response matches the expected employee ID
+    if 'id' not in user_request or user_request['id'] != employee_id:
+        print(f"Error: Employee with ID {employee_id} not found.")
+        sys.exit(1)
+
     user_name = user_request.get('username')
 
     filename = f'{employee_id}.csv'
+    tasks_list = []
+
+    for task in tasks:
+        task_dict = {
+            "USER_ID": employee_id,
+            "USERNAME": user_name,
+            "TASK_COMPLETED_STATUS": str(task['completed']),
+            "TASK_TITLE": task['title']
+        }
+        tasks_list.append(task_dict)
 
     with open(filename, mode='w', newline='') as csv_file:
-        csv_writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
-        csv_writer.writerow(
-            ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
-
-        for task in tasks:
-            csv_writer.writerow(
-                [employee_id, user_name,
-                 str(task['completed']), task['title']])
+        csv_writer = csv.DictWriter(
+            csv_file, fieldnames=["USER_ID", "USERNAME",
+                                  "TASK_COMPLETED_STATUS", "TASK_TITLE"])
+        csv_writer.writeheader()
+        csv_writer.writerows(tasks_list)
 
     print(f'Data exported to {filename}')
+    print(f'Number of tasks in CSV:\
+          {"OK" if len(tasks) == 20 else "Incorrect"}')
 
 
 def get_user_tasks(employee_id):
